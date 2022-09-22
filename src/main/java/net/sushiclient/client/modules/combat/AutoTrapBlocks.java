893f1d357b7bc3d1f6f5fc9ffb24ab27b84e64fe
hiyama283 <watamituofiical@gmail.com>
2022-09-23 02:08:56 +0900

    0.3.10
    Change AutoTrap(Add Option)

--- src/main/java/net/sushiclient/client/modules/combat/AutoTrapBlocks.java
+++ src/main/java/net/sushiclient/client/modules/combat/AutoTrapBlocks.java
@@ -1,20 +1,31 @@
 package net.sushiclient.client.modules.combat;
 
+import com.google.gson.annotations.SerializedName;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.sushiclient.client.config.data.Named;
 
-public enum AutoTrapBlocks {
-    OBSIDIAN(Blocks.OBSIDIAN),
-    ANVIL(Blocks.ANVIL),
+public enum AutoTrapBlocks implements Named {
+    @SerializedName("Obsidian")
+    OBSIDIAN(Blocks.OBSIDIAN, "Obsidian"),
+    @SerializedName("Anvil")
+    ANVIL(Blocks.ANVIL, "Anvil"),
     ;
 
 
     private final Block block;
-    AutoTrapBlocks(Block useBlock) {
+    private final String name;
+    AutoTrapBlocks(Block useBlock, String name) {
         block = useBlock;
+        this.name = name;
     }
 
     public Block getBlock() {
         return block;
+    }
+
+    @Override
+    public String getName() {
+        return name;
     }
 }
