27224bd17ce2c77db81845a524efa4ec77a591be
hiyama283 <watamituofiical@gmail.com>
2022-10-03 00:48:25 +0900

    Change name to Trojan client

--- sushi-client/src/main/java/net/sushiclient/client/ModInformation.java
+++ sushi-client/src/main/java/net/sushiclient/client/ModInformation.java
@@ -1,6 +1,6 @@
 package net.sushiclient.client;
 
 public class ModInformation {
-    public static final String name = "Shark sushi";
+    public static final String name = "Trojan client";
     public static final String version = "0.4";
 }
